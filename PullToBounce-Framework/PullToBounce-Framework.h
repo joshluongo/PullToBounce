f7ebc6f44256c18a8f2bf426e179fe307efa44b7
Joshua Luongo <josh@jrapps.com.au>
2016-06-03 23:18:58 +1000

    Attempt to fix Carthage build issue.

---
+++ PullToBounce-Framework/PullToBounce-Framework.h
@@ -0,0 +1,19 @@
+//
+//  PullToBounce-Framework.h
+//  PullToBounce-Framework
+//
+//  Created by Joshua Luongo on 3/06/2016.
+//  Copyright © 2016 Uniface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PullToBounce-Framework.
+FOUNDATION_EXPORT double PullToBounce_FrameworkVersionNumber;
+
+//! Project version string for PullToBounce-Framework.
+FOUNDATION_EXPORT const unsigned char PullToBounce_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PullToBounce_Framework/PublicHeader.h>
+
+
